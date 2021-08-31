f482ce8dba57d62867287f078f65cfba7dc849b0
Mayara Machado <79062281+Mayara008@users.noreply.github.com>
2021-08-30 23:37:31 -0300

    Add files via upload

---
+++ minhalojadegames/src/main/java/br/org/generation/minhalojadegames/model/Produto.java
@@ -0,0 +1,93 @@
+package br.org.generation.minhalojadegames.model;
+ 
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@Entity 
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id; 
+
+	@NotNull(message = "Obrigatório nome") 
+	@Size(min = 5, max = 255)
+	private String nome;
+
+	@NotNull(message = "Obrigatório Preço")
+	private BigDecimal preco; 
+	
+	@NotNull(message = "Obrigatório quantidade!")
+	private int quantidade; 
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto") 
+	private Produto produto;
+	
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+}
---
+++ minhalojadegames/src/main/java/br/org/generation/minhalojadegames/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package br.org.generation.minhalojadegames.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import org.generation.lojadegames.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	public List<Categoria> findAllByGeneroContainingIgnoreCase(String genero);
+
+}
